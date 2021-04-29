149b1ed424b9cff872997c4544bdb45b0034b0b5
Nathan Ho <nho21@uic.edu>
2021-04-29 01:39:01 -0500

    bug fix

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-import datetime, sqlite3, json, database 
+import datetime, sqlite3, json, database, os
 from flask import Flask, request
 from os import path
 
@@ -9,36 +9,42 @@ def create_DB():
     pass
 
 def create_entry():
-  # temp prototyping to make sure creating entries works
-  class_title = input("Enter a class: ")
-  assignment = input("Enter assignment name: ")
-  difficulty = input("Enter difficulty on scale 1-10: ")
-  status = input("Enter assignment status: ")
-  data = {}
-  data['assignments'] = []
-  ID = gen_ID()
-  data['assignments'].append({
-      'ID': ID + 1,
-      'class': class_title,
-      'assignment': assignment,
-      'difficulty': difficulty,
-      'status': status
-      })
-  print("success!")
-  if path.exists('data.json') == False:
-    with open('data.json', 'w') as outfile:
-      json.dump(data, outfile)
-  else:
-    with open('data.json', 'a') as outfile:
-      json.dump(data, outfile)
-  return outfile
-
-
+    # temp prototyping to make sure creating entries works
+    class_title = input("Enter a class: ")
+    assignment = input("Enter assignment name: ")
+    difficulty = input("Enter difficulty on scale 1-10: ")
+    status = input("Enter assignment status: ")
+    data = {}
+    data['assignments'] = []
+    ID = gen_ID()
+    data['assignments'].append({
+        'ID': ID + 1,
+        'class': class_title,
+        'assignment': assignment,
+        'difficulty': difficulty,
+        'status': status
+        })
+    print("success!")
+    if path.exists('data.json') == False:
+        with open('data.json', 'w') as outfile:
+            json.dump(data, outfile)
+    else:
+        with open('data.json', 'a') as outfile:
+            json.dump(data, outfile)
+    return outfile
 
 def gen_ID():
-  f = open('data.json')
-  json_file = json.load(f)
-  if json_file 
-  ID = len(json_file['assignments'])
-  return ID
+    file_size = os.path.getsize('data.json')
+    if file_size <= 2:
+        ID = 0
+    else:
+        f = open('data.json')
+        json_file = json.load(f)
+        for entry in json_file['assignments']:
+            print(entry)
+    return ID
+
+create_entry()
+
+def sql_brrr():
 
--- database.py
+++ database.py
@@ -1,33 +1,24 @@
 import sqlite3, json, datetime 
+import os
 from os import path
 
 def createRDB():
-  con = sqlite3.connect('assignments.db')
-  cur = con.cursor()
-  table_create = 'CREATE TABLE assigments (ID int, CLASS_TITLE text, ASSIGNMENT text, DIFFICULTY int, STATUS text)'
-  con.commit()
-  con.close()
+    con = sqlite3.connect('assignments.db')
+    cur = con.cursor()
+    table_create = 'CREATE TABLE assigments (ID int, CLASS_TITLE text, ASSIGNMENT text, DIFFICULTY int, STATUS text)'
+    con.commit()
+    con.close()
 
 def checkforDB():
-  if path.exists('assignments.db') == False:
-    createRDB()
-    return False
-  else:
-    return True
+    if path.exists('assignments.db') == False:
+        createRDB()
+        return False
+    else:
+        return True
 
 def create_entry_sql():
-  f = open('data.json')
-  json_file = json.load(f)
-  print(json_file['assignments'])
+    f = open('data.json')
+    json_file = json.load(f)
+    print(json_file['assignments'])
 
 create_entry_sql()
-  
-def create_entry_RDB():
-  json_file = open('data.json')
-  data = json.load(json_file)
-  for entry in data['class']:
-    print(entry)
-  json_file.close()
-
-create_entry_RDB()
-
