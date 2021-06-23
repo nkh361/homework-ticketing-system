31ca30cb6b694fea26b49fe34c8da8ac4a209f07
Nathan Ho <nho21@uic.edu>
2021-06-22 23:30:53 -0500

    crud crud crud

--- src/database.py
+++ src/database.py
@@ -1,11 +1,35 @@
 import sqlite3, json, os
 from os import path
+import pandas as pd
 
 sql_file = 'assignments.db'
 
 class SQL_entry():
-    def __init__(self):
+    def create_entry(self):
         with open('data.json', 'r') as f:
             data = json.loads(f.read())
         df_assignments = pd.json_normalize(data, record_path = ['assignments'])
-        ## To be continued
+        conn = sqlite3.connect(sql_file)
+        df_assignments.to_sql('assignments', conn, if_exists = 'replace')
+
+    def read_entry(self):
+        conn = sqlite3.connect(sql_file)
+        cur = conn.cursor()
+        sql_statement = "SELECT * FROM assignments"
+        query = cur.execute(sql_statement)
+        return query.fetchall()
+    
+    def update_entry(self, ID):
+        conn = sqlite3.connect(sql_file)
+        cur = conn.cursor()
+        sql_statement = "SELECT * FROM assignments WHERE ID={}".format(ID)
+        query = cur.execute(sql_statement)
+        update = input("What would you like to update: 1 - assignment 2 - due date 3 - priority: ")
+        if update = "1":
+            sql_statement = "UPDATE assignments SET assignment = {} WHERE ID = {}".format(
+
+def main():
+    a = SQL_entry()
+    a.update_entry(1)
+
+main()
