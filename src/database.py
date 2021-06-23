cdfe680a6720ebe59f09e4c44f65a645cb9d8324
Nathan Ho <nho21@uic.edu>
2021-06-22 23:51:38 -0500

    more crud :/

--- src/database.py
+++ src/database.py
@@ -15,21 +15,28 @@ class SQL_entry():
     def read_entry(self):
         conn = sqlite3.connect(sql_file)
         cur = conn.cursor()
-        sql_statement = "SELECT * FROM assignments"
+        sql_statement = 'SELECT * FROM assignments'
         query = cur.execute(sql_statement)
         return query.fetchall()
-    
+
+   
     def update_entry(self, ID):
         conn = sqlite3.connect(sql_file)
         cur = conn.cursor()
         sql_statement = "SELECT * FROM assignments WHERE ID={}".format(ID)
         query = cur.execute(sql_statement)
         update = input("What would you like to update: 1 - assignment 2 - due date 3 - priority: ")
-        if update = "1":
-            sql_statement = "UPDATE assignments SET assignment = {} WHERE ID = {}".format(
-
+        if update == "1":
+            assignment_updated_entry = input("Enter updated assignment: ")
+            sql_statement_assignment_update = 'UPDATE assignments SET assignment = "{}" WHERE ID = {}'.format(assignment_updated_entry, ID)
+            query = cur.execute(sql_statement_assignment_update)
+        if update == "2":
+            due_date_updated_entry = input("Enter updated due date: ")
+            sql_statement_due_date_update = 'UPDATE assignments SET "Due Date" = {} WHERE ID = {}'.format(due_date_updated_entry, ID)
+        print(self.read_entry())
+    
 def main():
     a = SQL_entry()
-    a.update_entry(1)
+    a.update_entry(2)
 
 main()
