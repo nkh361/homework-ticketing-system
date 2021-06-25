7c890927c960578bdaf6975656d00b212b1a589c
nkh361 <nho21@uic.edu>
2021-06-25 00:48:49 -0500

    finished : update_entry, IP : delete_entry

--- src/database.py
+++ src/database.py
@@ -23,18 +23,35 @@ class SQL_entry():
     def update_entry(self, ID):
         conn = sqlite3.connect(sql_file)
         cur = conn.cursor()
-        sql_statement = "SELECT * FROM assignments WHERE ID={}".format(ID)
-        query = cur.execute(sql_statement)
+        # sql_statement = "SELECT * FROM assignments WHERE ID={}".format(ID)
+        # query = cur.execute(sql_statement)
         update = input("What would you like to update: 1 - assignment 2 - due date 3 - priority: ")
         if update == "1":
             assignment_updated_entry = input("Enter updated assignment: ")
-            sql_statement_assignment_update = 'UPDATE assignments SET assignment = "{}" WHERE ID = {}'.format(assignment_updated_entry, ID)
-            query = cur.execute(sql_statement_assignment_update)
+            sql_assignment_updated = 'UPDATE assignments SET assignment = "{}" WHERE ID = {}'.format(assignment_updated_entry, str(ID))
+            query = cur.execute(sql_assignment_updated)
+            conn.commit()
         if update == "2":
             due_date_updated_entry = input("Enter updated due date: ")
-            sql_statement_due_date_update = 'UPDATE assignments SET "Due Date" = {} WHERE ID = {}'.format(due_date_updated_entry, ID)
+            sql_due_date_update = 'UPDATE assignments SET "Due Date" = "{}" WHERE ID = {}'.format(due_date_updated_entry, str(ID))
+            query = cur.execute(sql_due_date_update)
+            conn.commit()
+        if update == "3":
+            priority_updated_entry = input("Entere updated priority: ")
+            sql_priority_updated = 'UPDATE assignments SET priority = "{}" WHERE ID = {}'.format(priority_updated_entry, str(ID))
+            query = cur.execute(sql_priority_updated)
+            conn.commit()
         print(self.read_entry())
     
+    def delete_entry(self, ID):
+        """
+        conn = sqlite3.connect(sql_file)
+        cur = conn.cursor()
+        delete = input("What would you like to delete")
+        """
+        pass
+
+
 def main():
     a = SQL_entry()
     a.update_entry(2)
