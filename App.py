49cfb89c4b983acac2fc2a8477deaeadb72fb920
nkh361 <nho21@uic.edu>
2021-07-06 00:21:40 -0500

    werrkkk

--- App.py
+++ App.py
@@ -1,50 +1,10 @@
-import json_file, database, os
+from src.json_object import json_object
+from src.database import SQL_entry
 from os import path
 from flask import Flask, request, render_template
 
-def check_for_db():
-    if os.path.exists('assignments.db') == False:
-        print("hazzah!")
-    else:
-        print("poo")
+class Server:
+    def __init__(self):
+        self.test = "Hello, world!"
 
-def create_entry():
-    """
-    creates entries for both JSON and SQL table 'assignments'
-    """
-    json_file.create_entry_json()
-    database.create_entry_sql()
-
-app = Flask(__name__)
-@app.route('/')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/', methods = ['POST'])
-def display():
-    """
-    returns SQL query to client
-    """
-    data_db = database.display_all_assignments()
-    # data_str = str()
-    """
-    for i in range(len(data_db)):
-        for entry in data_db:
-            data_str += str(entry[i]) + ' , '
-        if i == 7:
-            data_str += '\n'
-            i = 0
-    """
-    return render_template('index.html', data = data_db)
-
-def main():
-    # create_entry()
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug = True, use_reloader = True)
-
-main()
-
-
+from flask import
