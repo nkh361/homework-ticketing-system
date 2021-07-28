c73982436b017d8f587fc4f0e1ff79b6c2840472
Nathan Ho <nho21@uic.edu>
2021-07-28 15:36:51 -0500

    daily progress

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ from src.json_object import json_object
 from src.database import SQL_entry
 from os import path
 from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
@@ -26,9 +27,16 @@ def test_json():
 """
 TODO:
     create SQL entry for assignments
-    list assignments to user
-    sort by priority
+    fix show_data()
+        not showing json stuff
+    or
+        can query from database and display data from there
 """
+
+@app.route('/all.html', methods = ['GET'])
+def show_data():
+    json_file = 'data.json'
+    return render_template('all.html', title="tickets", jsonfile=json.dumps(json_file))
 
 def main():
     app.config['TEMPLATES_AUTO_RELOAD'] = True
