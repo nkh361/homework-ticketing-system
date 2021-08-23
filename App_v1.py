8f8b319f32edd99e2832195fbca74a12434900de
Nathan Ho <nho21@uic.edu>
2021-08-22 21:09:10 -0500

    fixed sql table

--- App_v1.py
+++ App_v1.py
@@ -16,12 +16,6 @@ class User:
     def __repr__(self):
         return f'<User: {self.username}>'
 
-    def user_table_genisis(self):
-        conn = sqlite3.connect(relational_database)
-        cur = conn.cursor()
-        query = cur.execute('CREATE TABLE Users (ID int, username text, password text)')
-        conn.commit()
-
     def user_table_count(self):
         conn = sqlite3.connect(relational_database)
         cur = conn.cursor()
@@ -32,18 +26,15 @@ class User:
     def create_table_for_users(self):
         conn = sqlite3.connect(relational_database)
         cur = conn.cursor()
-
-        if self.user_table_count() == 0 or '0':
-            sql_statement = 'CREATE TABLE Users (ID int, username text, password text)'
-            query = cur.execute(sql_statement)
-            
+        sql_statement = "CREATE TABLE Users ('ID' int, 'username' text, 'password' text)"
+        query = cur.execute(sql_statement)           
         conn.commit()
 
     def create_user_entry(self):
         conn = sqlite3.connect(relational_database)
         cur = conn.cursor()
-        sql = "INSERT INTO Users (ID, username, password) VALUES (%d, %s, %s)" % (self.ID, self.username, self.password)
-        cur.execute(sql)
+        sql = "INSERT INTO Users (ID, username, password) VALUES (:ID, :username, :password)"
+        cur.execute(sql, {"ID":self.ID, "username":self.username, "password":self.password})
         conn.commit()
 
     def get_all_users(self):
@@ -58,6 +49,8 @@ class User:
 users = []
 users.append(User(ID = 1, username = 'nathan', password = 'test'))
 users.append(User(ID = 2, username = 'nkh', password = 'nkh'))
+
+# send the list of users to database
 
 @app.before_request
 def before_request():
@@ -100,11 +93,8 @@ def profile():
 def main():
     # app.config['TEMPLATES_AUTO_RELOAD'] = True
     # app.run(debug = True, use_reloader = False)
-    user = User(ID = 3, username = 'test', password = 'test')
-    """
-    no such column: test
-    """
-    # user.user_table_genisis()
+    user = User(ID = 3, username = "nkh", password = "test")
+    user.create_table_for_users()
     user.create_user_entry()
 
 main()
