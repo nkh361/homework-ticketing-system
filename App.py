18c17e5e3b0ec3e0ce18d8b791510643492f2d3c
nkh361 <nho21@uic.edu>
2022-11-02 22:05:12 -0500

    progress

--- App.py
+++ App.py
@@ -1,14 +1,73 @@
 from src.json_object import json_object
-from src.database import SQL_entry
 from os import path
-from flask import Flask, request, render_template, jsonify
-import json
+from flask import Flask, request, render_template, jsonify, redirect, url_for, g, abort
+import json, sqlite3, os, datetime
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy_session import flask_scoped_session
+from sqlalchemy.dialects.postgresql import JSON
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'test'
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+# reload the user object from the user ID stored in the session
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    # ticket_data = db.Column(JSON, nullable=True)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+
+        if existing_user_username:
+            raise ValidationError("The username is taken.")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    print("Current: ", current_user)
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route("/")
 def root_page():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/', methods = ['POST'])
 def test_json():
@@ -23,6 +82,8 @@ def test_json():
     json.send_to_json()
 
     return "Entered Successfully!"
+
+
 
 @app.route('/all.html', methods = ['GET'])
 def show_data():
--- src/json_object.py
+++ src/json_object.py
@@ -1,67 +1,12 @@
-import sqlite3, json, os
-import pandas as pd
+import json, os
 from datetime import date
 from os import path
 from dataclasses import dataclass
 
 @dataclass
-class json_object:
+class ticket:
     username: str
     assignment: str
     start_date: str
     due_date: str
     priority: str
-    
-    def send_to_json(self):
-        weights = {"low": 1, "mid": 3, "high": 5, "Not assigned": 0}
-        data = {}
-        data['assignments'] = []
-
-        def get_weight():
-            return weights[self.priority]
-
-        try:
-            if path.exists('data.json') == False:
-                with open('data.json', 'w') as outfile:
-                    ID = self.gen_ID()
-                    # possibly use uuid, just send to file and check if its in
-                    data['assignments'].append({
-                        'Username': self.username,
-                        'ID': ID + 1,
-                        'Assignment': self.assignment,
-                        'Start Date': self.start_date,
-                        'Due Date': self.due_date,
-                        'Priority': get_weight()
-                        })
-                    outfile.seek(0) # reset the file pointer to index 0
-                    json.dump(data, outfile, indent=4)
-
-            else:
-                with open('data.json', 'r+') as outfile:
-                    ID = self.gen_ID()
-                    json_dict = json.load(outfile)
-                    json_dict['assignments'].append({
-                        'Username': self.username,
-                        'ID': ID + 1,
-                        'Assignment': self.assignment,
-                        'Start Date': self.start_date,
-                        'Due Date': self.due_date,
-                        'Priority': get_weight()
-                    })
-                    outfile.seek(0)
-                    json.dump(json_dict, outfile, indent=4)
-                    print("success!")
-        except:
-            print("json push error")
-
-        
-    def gen_ID(self):
-        if (path.exists('data.json') == False) or (os.path.getsize('data.json')<=2):
-            return 0 # json file DNE or empty json file
-        else:
-            f = open('data.json')
-            json_file = json.load(f)
-            for element in json_file['assignments']:
-                ID = element['ID']
-            return ID
-
