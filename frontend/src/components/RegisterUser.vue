bc21890d444e4a25de5a14082326045b8c01d0f9
nkh361 <nho21@uic.edu>
2024-02-23 21:54:55 -0600

    TODO: fix redirect for registration. Added router.

---
+++ frontend/src/components/RegisterUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="RegisterUser">
+    "Welcome"
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterUser',
+  props: {
+
+  }
+}
+</script>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import RegisterUser from './components/RegisterUser.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: HelloWorld },
+  { path: '/register', component: RegisterUser}
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
